0636d606d7df3103652b7936a5ff2d9b40f6e843
Andrew D Urquhart <andrewurq@gmail.com>
2022-04-20 16:43:03 -0400

    :tada: testing out basic typescript functionality

---
+++ test.js
@@ -0,0 +1,15 @@
+// just teaching myself typescript
+console.log('hello world i am typescript');
+// tsc test.ts
+async function hello() {
+    return 'world';
+}
+// tsc test.ts  ----- craziness ensues
+// touch tsconfig.json
+// {
+//     "compilerOptions": {
+//         "target": "esnext",
+//         "watch": true
+//     }
+// }
+const url = new URL();
---
+++ test.ts
@@ -0,0 +1,19 @@
+// just teaching myself typescript
+
+console.log('hello world i am typescript')
+// tsc test.ts
+
+async function hello() {
+    return 'world'
+}
+// tsc test.ts  ----- craziness ensues
+// touch tsconfig.json
+
+// {
+//     "compilerOptions": {
+//         "target": "esnext",
+//         "watch": true
+//     }
+// }
+
+const url = new URL()
